9ba5d0ad90ab6340297fa8aea31027bf2c9debb7
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2024-02-28 02:10:13 -0500

    Inicie con la documentacion

--- hotel/urls.py
+++ hotel/urls.py
@@ -17,9 +17,26 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Hotel Api",
+      default_version='v1',
+      description="Api para la gestion de Reservas, Clientes y habitaciones",
+      contact=openapi.Contact(email="r.apaza.cornejo@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('habitaciones/', include('habitaciones.urls')),
     path('clientes/', include('clientes.urls')),
-    path('reservas/',include('reservas.urls'))
+    path('reservas/',include('reservas.urls')),
+    
+    path('documentacion/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
