56d24c967c95f4c91f9c9bcab5e62a1bfab2cbce
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2024-02-27 21:03:33 -0500

    creacion del modelo reservas

--- hotel/settings.py
+++ hotel/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'habitaciones',
     'clientes',
+    'reservas',
 ]
 
 MIDDLEWARE = [
--- reservas/models.py
+++ reservas/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from habitaciones.models import Habitacion
+from clientes.models import Cliente
 
 # Create your models here.
+class Reserva(models.Model):
+    id = models.AutoField(primary_key=True , null=False)
+    cliente = models.ForeignKey(to=Cliente,
+                                on_delete=models.PROTECT)
+    habitacion = models.ForeignKey(to=Habitacion,
+                                   on_delete=models.PROTECT)
+    ESTADO_CHOICES = (
+        ('pendiente', 'Pendiente'),
+        ('confirmada', 'Confirmada'),
+        ('pagada', 'Pagada'),
+        ('cancelada', 'Cancelada'),
+        ('finalizada', 'Finalizada'),
+    )
+    
+    estado = models.CharField(choices=ESTADO_CHOICES , default='pendiente')
+    metodo_de_pago = models.CharField(null=False)
+    hora_ingreso = models.DateTimeField(auto_now=True)
+    hora_salida = models.DateTimeField(blank=True, null=True)
+    
+    class Meta:
+        db_table = 'reservas'
