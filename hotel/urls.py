901290ec810c9b59e6b3ea9819a19e9526d09a89
Roberto <r.apaza.cornejo@gmail.com>
2024-03-04 10:03:52 -0500

    manejos de archivos multimedia listo

--- hotel/urls.py
+++ hotel/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -39,4 +42,4 @@ urlpatterns = [
     
     path('documentacion/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
