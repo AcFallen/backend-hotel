c89d0cdec8b4b763a6c47cf4af2755398ef59c66
Roberto <r.apaza.cornejo@gmail.com>
2024-03-07 09:53:07 -0500

    agregue variable staticos

--- hotel/settings.py
+++ hotel/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -161,3 +162,5 @@ SWAGGER_SETTINGS = {
     },
     'USE_SESSION_AUTH': False,
 }
+
+STATIC_ROOT = BASE_DIR / 'estaticos'
