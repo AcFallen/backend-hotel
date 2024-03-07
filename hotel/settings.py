a5ac7f057ba01981fcc00918bdcfecd0745fbdf8
Roberto <r.apaza.cornejo@gmail.com>
2024-03-07 18:10:13 -0500

    cambiando a estado normal

--- hotel/settings.py
+++ hotel/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%&=(5y)&q#mks0llgruschv#24iyv*^sr0qd#6s9&x2@jycy_v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['hotel-qa7p.onrender.com' ,'*']
 
