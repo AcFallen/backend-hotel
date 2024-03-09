f345bd867e4d82c47725a1de8b8aea4119020f64
Roberto <r.apaza.cornejo@gmail.com>
2024-03-09 06:56:04 -0500

    cambie configuracion para poder usar filtros en mis views

--- hotel/settings.py
+++ hotel/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'drf_yasg',
     'habitaciones',
     'clientes',
@@ -153,7 +154,8 @@ MEDIA_ROOT = BASE_DIR / 'archivos'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend')
     
 }
 
