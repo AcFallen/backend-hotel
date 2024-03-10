4def198dc627ebcc28e3cd401a115a28b9f75242
Roberto <r.apaza.cornejo@gmail.com>
2024-03-10 08:34:29 -0500

    agregue serializador para emitir boletas

--- reservas/serializer.py
+++ reservas/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Reserva
 from habitaciones.serializer import HabitacionSerializer
 from clientes.serializer import ClienteSerializer
+from habitaciones.serializer import HabitacionSerializer
 
 class ReservaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,3 +21,19 @@ class ActulizarEstadoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reserva
         fields =  ['estado']
+
+
+#-------------------------------------------------------------------#
+
+
+class GenerarBoletaSerializer(serializers.ModelSerializer):
+    documento_usuario=serializers.CharField(max_length = 8 , min_length=8 , allow_null = False , required =True)
+    nombre_usuario = serializers.CharField(required =True)
+    cantidad_dias = serializers.IntegerField( min_value = 0) 
+    habitacion_id = serializers.IntegerField( min_value = 0)
+
+    class Meta :
+        model = Reserva
+        #fields = '__all__'
+        exclude = ['metodo_de_pago' , 'clienteId' , 'habitacionId']
+
